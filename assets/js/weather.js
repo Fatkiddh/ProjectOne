02df5ac0e4e82477a7932c898ed7792bfaa2bcad
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-01-20 14:38:31 -0800

    staying up to date

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,58 +1,19 @@
-let apiKey = `e73f0588b3464897830947746d60b65d`;
-// Cheat-Sheet
-/*          {
-             "timezone":"America\/New_York",
-             "state_code":"NC",
-             "lat":35.7721,
-             "lon":-78.63861,
-             "country_code":"US",
-             "station_id":"723060-13722",
-             "data":[
-                {
-                   "rh":32,
-                   "wind_spd":6.7,
-                   "slp":1020.3,
-                   "h_angle":-90,
-                   "dewpt":-7.5,
-                   "snow":0,
-                   "uv":0,
-                   "wind_dir":220,
-                   "weather":{
-                      "icon":"c01n",
-                      "code":"800",
-                      "description":"Clear sky"
-                   },
-                   "pod":"n",
-                   "vis":1.5,
-                   "precip":0,
-                   "elev_angle":-33,
-                   "ts":1483232400,
-                   "pres":1004.7,
-                   "datetime":"2017-01-01:04",
-                   "temp":8.3,
-                   "dhi":0,
-                   "clouds":0
-                }, ...
-             ],
-             "city_name":"Raleigh",
-             "city_id":"4487042"
-          } */
+let luigiKey = `53377696a03f2e041c102d0eefcf9db8`;
+let kelvinMax = ``;
+let kelvinMin = ``;
 // Interpolate these. Wait on Ryan's function or help structure one to take both.
-let city = ``;
-let startDate = ``;
-let endDate = ``;
-
+let city ="";
 const getWeather = () => {
-
+  console.log('click');
   $.ajax({
-    url: `https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=${apiKey}`,
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${city},3166-2&APPID=${luigiKey}`,
     method: `GET`,
   }).done(function response(response){
-    console.log(response);
-    let high = ``;
-    let low = ``;
+    kelvinMax = response.list[0].main.temp_max;
+    kelvinMin = response.list[0].main.temp_min;
+    let high = $(``).text((kelvinMax-273.15)*1.8+32);
+    let low = $(``).text((kelvinMin-273.15)*1.8+32);
   });
-
-}// Closes getWeather function
+}// Closes getWeat`her function
 getWeather();
 console.log(`hello you!`);
